6c181544702cb91a94314c1ad692d3d4846eb353
Peter Cerhan <petercerhan@gmail.com>
2026-06-19 13:52:29 -0500

    Delete old experimental Container Fragment code

--- app/src/main/java/com/example/navigationapplication/page_two/PageTwoFragment.kt
+++ app/src/main/java/com/example/navigationapplication/page_two/PageTwoFragment.kt
@@ -6,9 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.navigationapplication.R
 import com.example.navigationapplication.controller_library.SceneFragment
-import com.example.navigationapplication.level_two.ContainerFragment
-import com.example.navigationapplication.level_two.InitialScreen
-import java.util.UUID
 
 class PageTwoFragment : SceneFragment<PageTwoViewModel>() {
 
@@ -31,25 +28,4 @@ class PageTwoFragment : SceneFragment<PageTwoViewModel>() {
         viewModel.back()
     }
 
-
-
-    //Legacy Navigation Functions//
-
-    private fun navigateToLevelTwoContainer() {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .setCustomAnimations(
-                R.anim.fragment_slide_in_bottom,
-                0,
-                0,
-                R.anim.fragment_slide_out_bottom
-            )
-            .add(
-                R.id.main,
-                ContainerFragment.newInstance(
-                    InitialScreen.LEVEL_TWO_PAGE_ONE,
-                    UUID.randomUUID() //placeholder to compile; shouldn't need to pass a coordinator ID
-                )
-            )
-            .commit()
-    }
 }
